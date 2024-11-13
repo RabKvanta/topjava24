4b30cb6df218769292c4167f18fa143775f2be7b
MainUser <niikvan.kiev@gmail.com>
2024-11-13 16:47:04 +0200

    HW01 create table

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalTime;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -21,7 +22,8 @@ public class MealServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("getAll");
-        //request.setAttribute("meals", MealsUtil.meals);
+        request.setAttribute("meals", MealsUtil.filteredByStreams(MealsUtil.meals,
+                                            LocalTime.of(0, 0), LocalTime.of(23, 59), 2000));
         request.getRequestDispatcher("/meals.jsp").forward(request, response);
 //        response.sendRedirect("meals.jsp");
     }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,13 +1,38 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html lang="ru">
 <head>
     <title>Meals</title>
+    <style>
+        .normal {
+            color: green;
+        }
+
+        .exceeded {
+            color: red;
+        }
+    </style>
 </head>
 <body>
 <h3><a href="index.html">Home</a></h3>
 <hr>
 <h2>Meals</h2>
-<table >
+<table border="1" cellspacing="1" cellpadding="1">
+    <tr> <th>Date</th>
+    <th>Description</th>
+    <th>Calories</th>
+    </tr>
+    <c:forEach items="${meals}" var="um">
+        <tr>
+            <td>
+              ${um.dateTime}
+            </td>
+            <td> ${um.description}</td>
+            <td> ${um.calories}</td>
+        </tr>
+    </c:forEach>
+
 </table>
 </body>
 </html>
